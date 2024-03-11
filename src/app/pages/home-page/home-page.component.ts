72b9f65b21be894386b54e1e689418774fe83975
Elena Shkaporova <elenashkaporova@MacBook-Air-Elena.local>
2024-03-11 17:00:05 +0400

    created mock data and dynamic behavior in account div

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../components/ui/container/container.component';
 import { HeaderComponent } from '../../components/ui/header/header.component';
 import { PlateComponent } from '../../components/ui/plate/plate.component';
+import { MatIconModule } from '@angular/material/icon';
+import { Balance, balances } from './data';
+
 
 @Component({
   selector: 'ns-home-page',
   standalone: true,
-  imports: [ContainerComponent, HeaderComponent, PlateComponent],
+  imports: [ContainerComponent, HeaderComponent, PlateComponent, MatIconModule, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  balances = balances;
+  selectedBalance: Balance | null = null;
+  isActive = false;
+  ngOnInit() {
+    this.selectedBalance = balances[0];
+  }
 
+  toggleClass() {
+    this.isActive = !this.isActive;
+  }
+  selectBalance(balance: Balance) {
+    this.selectedBalance = balance;
+    this.toggleClass();
+  }
 }
